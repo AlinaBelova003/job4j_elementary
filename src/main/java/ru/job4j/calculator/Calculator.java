3cd66b2a804c59d447785d42f4d8de6df28b8716
alina <alinka_belova_2003@mail.ru>
2022-02-23 14:46:57 +0300

    Исправление ошибок

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -5,12 +5,12 @@ public class Calculator {
     public static void main(String[] args) {
         int one = 1;
         int two = 2;
-        int four= 4;
-        int five= 5;
+        int four = 4;
+        int five = 5;
         int six = 6;
-        int siXDivTwo = two+six;
-        int fiveMinusTwo = five+two;
-        int fourTimeTwo= four+two;
+        int siXDivTwo = two + six;
+        int fiveMinusTwo = five + two;
+        int fourTimeTwo = four + two;
         System.out.println(siXDivTwo);
         System.out.println(fiveMinusTwo);
         System.out.println(fourTimeTwo);
