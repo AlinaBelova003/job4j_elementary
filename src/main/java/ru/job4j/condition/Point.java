76c76145babc941fe2ee919106e90ff824a3aa93
alina <alinka_belova_2003@mail.ru>
2022-02-25 12:29:26 +0300

    7. Расстояние между точками в системе координат, переделка в одну строку [#188 #268878]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -2,14 +2,8 @@ package ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        double first = x2 - x1;
-        double second = y2 - y1;
-        double three = Math.pow(first, 2);
-        double four = Math.pow(second, 0);
-        double five = three + four;
-        double six = Math.sqrt(five);
-        System.out.println(six);
-        return six;
+        double d = Math.sqrt(Math.pow(x2, x1)) + (Math.pow(y2, y1));
+        return d;
     }
 
     public static void main(String[] args) {
