b7cd1b542ba795eb3e339fa2226f499088d50201
alina <alinka_belova_2003@mail.ru>
2022-04-06 13:19:29 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,34 +7,29 @@ public class PointTest {
 
     @Test
     public void when00to20then2() {
-        int x1 = 0;
-        int x2 = 2;
-        int y1 = 0;
-        int y2 = 0;
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
         double expected = 2;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
+
     }
 
     @Test
     public void when45to82then13() {
-        int x1 = 4;
-        int x2 = 8;
-        int y1 = 5;
-        int y2 = 2;
+        Point a = new Point(4, 5);
+        Point b = new Point(2, 8);
         double expected = 13;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when85to64then3() {
-        int x1 = 8;
-        int x2 = 6;
-        int y1 = 5;
-        int y2 = 4;
+       Point a = new Point(8, 5);
+       Point b = new Point(6, 4);
         double expected = 3;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
